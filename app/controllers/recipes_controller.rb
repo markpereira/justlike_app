e424574bd8adcde21e39b71ad3de54208b80c06b
Mathilda Thompson <mathildathompson288@gmail.com>
2014-03-30 12:10:00 +1100

    Revisions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -38,43 +38,14 @@ class RecipesController < ApplicationController
     @status = "no search"
     search = params[:search]
     if search.present?
-      @recipes = Recipe.where('name ilike ?', "%#{ params[:search]}%")
+      @recipes = Recipe.where('name ilike ?', "%#{search}%")
     end 
   end
 
   #search yummly database for recipes and makes a copy for local search function
   
   def search_yummly
-    search = params[:search]
-    search = URI.escape(search)
-        app_key = "43d68684682bf636cfc259b4c36c275c"
-        app_id = "94c36fdb"
-        url = "http://api.yummly.com/v1/api/recipes?_app_id=#{app_id}&_app_key=#{app_key}&q=#{search}&maxResult=10&start=10"
-        @search_result = HTTParty.get(url)
-        
-        if ["Error"] == "Recipe not found!"
-          @status = "no results"
-        else
-        @status = "found results" 
-        @search_result["matches"].each do |result|
-          recipe = Recipe.new
-          recipe.yummly_id = result["id"]
-          recipe.name = result["recipeName"]
-          recipe.image = result["smallImageUrls"].first
-          recipe.rating = result["rating"]
-          recipe.servingsize = result["numberOfServings"]
-          recipe.cooktime = result["totalTimeInSeconds"]
-          recipe.save
-          result["ingredients"].each do |ingredient_result|
-            ingredient = Ingredient.new  
-            ingredient.name = ingredient_result
-            ingredient.save
-            recipe.ingredients << ingredient 
-           end 
-        end
-      end
-      recipes = Recipe.where('name ilike ?', "%#{ params[:search]}%")
-      @recipes = recipes.reverse
+    @recipes = Recipe.yummly(params[:search])
   end 
 
   #adding recipes to your dashboard favourites tab
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -22,4 +22,37 @@ class Recipe < ActiveRecord::Base
 	has_and_belongs_to_many :ingredients
 	
 	validates :yummly_id, :uniqueness => true
+
+    def self.yummly(search)
+        search = URI.escape(search)
+        app_key = "43d68684682bf636cfc259b4c36c275c"
+        app_id = "94c36fdb"
+        url = "http://api.yummly.com/v1/api/recipes?_app_id=#{app_id}&_app_key=#{app_key}&q=#{search}&maxResult=10&start=10"
+        @search_result = HTTParty.get(url)
+        if @search_result['matches'] == []
+          nil
+          #If the search returns no matches, the self.yummly method will return nil;
+        else
+            @status = "found results" 
+            @search_result["matches"].each do |result|
+                recipe = Recipe.new
+                recipe.yummly_id = result["id"]
+                recipe.name = result["recipeName"]
+                recipe.image = result["smallImageUrls"].first
+                recipe.rating = result["rating"]
+                recipe.servingsize = result["numberOfServings"]
+                recipe.cooktime = result["totalTimeInSeconds"]
+                recipe.save
+                result["ingredients"].each do |ingredient_result|
+                    ingredient = Ingredient.new  
+                    ingredient.name = ingredient_result
+                    ingredient.save
+                    recipe.ingredients << ingredient 
+                end 
+            #If the search does return results the method will return an array of recipes with the search name in the name;
+            end
+            recipes = Recipe.where('name ilike ?', "%#{search}%")
+            @recipes = recipes.reverse
+        end
+    end
 end	
